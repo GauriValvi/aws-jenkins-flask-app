33e0c31e2f6fee689ed269d95194219920162116
Ubuntu <ubuntu@ip-172-31-0-182.ap-south-1.compute.internal>
2026-09-10 13:51:06 +0000

    Update application message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from my first AWS DevOps pipeline!"
+    return "Hello from my first AWS DevOps pipeline!!!"
     
 
 @app.route("/health")
